6ed9682e07391f9a202d678f596736dc835fef22
axelll030 <kkravcovs@gmail.com>
2026-05-04 20:45:08 +0300

    Add tailwind, daisyui setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,16 @@
-function App() {
+import { Route, Routes } from "react-router";
+import { CreatePage, HomePage, NoteDetailPage } from "./pages";
+
+const App = () => {
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <div data-theme="forest">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreatePage />} />
+        <Route path="/note/:id" element={<NoteDetailPage />} />
+      </Routes>
+    </div>
   );
-}
+};
 
 export default App;
